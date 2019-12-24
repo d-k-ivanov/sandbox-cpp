d398cc6b5ff49fc323ab11646e888d7b8b721e25
Dmitriy Ivanov <d.k.ivanov@live.com>
2019-12-24 21:14:06 +0100

    add function for basic primarity check and diverse nubmer to a prime factors

---
+++ lib/primes.cpp
@@ -0,0 +1,65 @@
+/*
+ * =====================================================================
+ *      Project :  project-euler
+ *      File    :  3.cpp
+ *      Created :  20.12.2019 23:55:30 +0100
+ *      Author  :  Dmitriy Ivanov
+ *
+ *      The Problem:
+ *      The prime factors of 13195 are 5, 7, 13 and 29.
+ *      What is the largest prime factor of the number 600851475143 ?
+ *
+ * =====================================================================
+ */
+
+#include "logger.h"
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+namespace euler
+{
+
+    bool is_prime_slow(const unsigned long long n)
+     {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        for (unsigned long long i = 3; i < n/2; i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    std::vector<unsigned long long> get_prime_factors(unsigned long long n)
+    {
+        std::vector<unsigned long long> prime_factors;
+
+        LOG(euler::logger::log_level) << "DEBUG: init n=" << n << "\n";
+        // for (int i = 2; n == 0; i++)
+        unsigned long long i = 2;
+        while (true)
+        {
+            if (n % i == 0 && is_prime_slow(i))
+            {
+                LOG(euler::logger::log_level) << "DEBUG: i=" << std::setw(7) << i << " n = " << std::setw(14) << n << "\n";
+                prime_factors.push_back(i);
+                n = n / i;
+            }
+            if (n == 1)
+                break;
+            i++;
+        }
+        LOG(euler::logger::log_level) << "DEBUG: final n=" << n << "\n";
+
+        std::sort(prime_factors.begin(), prime_factors.end());
+        return prime_factors;
+    }
+}
