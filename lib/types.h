fc6837c82970a7ac1b198b5f07876abb6e06dfcf
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-01-07 11:52:50 +0100

    add type Uint64 == ULL

---
+++ lib/types.h
@@ -0,0 +1,16 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+/*
+ * =====================================================================
+ *      Project :  project-euler
+ *      File    :  types.h
+ *      Created :  07.01.2020 1:09:32 +0100
+ *      Author  :  Dmitriy Ivanov
+ * =====================================================================
+ */
+
+typedef unsigned long long Uint64;
+
+#endif //TYPES_H
+
