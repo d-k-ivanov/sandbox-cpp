eeece46c5764d26e906f49eb61d9dda5f26806c4
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-10-08 17:49:47 +0300

    Conan Essiantials: consumer task

---
+++ conan_essentials/consumer/builld_history.ps1
@@ -0,0 +1,9 @@
+conan remove --locks
+conan install . -pr ..\profile-vs2019-x64-static.txt --build=missing
+
+# Remove-Item -Recurse -Force ./_build
+New-Item _build -ItemType Directory -ErrorAction SilentlyContinue
+Set-Location _build
+
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE="Release"
+cmake --build . --config "Release"
