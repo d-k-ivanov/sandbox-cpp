5dbe216415150aa095ca80b7ea5b87d2d016025a
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-10-08 17:55:46 +0300

    Conan Essiantials: update gitignore and build command history

--- conan_essentials/consumer/builld_history.ps1
+++ conan_essentials/consumer/builld_history.ps1
@@ -1,5 +1,6 @@
 conan remove --locks
 conan install . -pr ..\profile-vs2019-x64-static.txt --build=missing
+conan search
 
 # Remove-Item -Recurse -Force ./_build
 New-Item _build -ItemType Directory -ErrorAction SilentlyContinue
