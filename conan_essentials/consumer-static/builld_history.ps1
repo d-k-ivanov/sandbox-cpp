6eea1bfc7a33288832e47848fe5afc39af8b0214
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-10-08 21:45:34 +0300

    Conan Essiantials: add visual studio code properties

--- conan_essentials/consumer-static/builld_history.ps1
+++ conan_essentials/consumer-static/builld_history.ps1
@@ -2,9 +2,9 @@ conan remove --locks
 conan install . -pr ..\profile-vs2019-x64-static.txt --build=missing
 conan search
 
-# Remove-Item -Recurse -Force ./_build
-New-Item _build -ItemType Directory -ErrorAction SilentlyContinue
-Set-Location _build
+# Remove-Item -Recurse -Force ./build
+New-Item build -ItemType Directory -ErrorAction SilentlyContinue
+Set-Location build
 
 cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE="Release" ..
 cmake --build . --config "Release"
