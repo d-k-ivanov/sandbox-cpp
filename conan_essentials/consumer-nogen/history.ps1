0c56d71332941a5e8a6f5da4b14683209dc65ab6
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-10-11 02:48:22 +0300

    Conan Essiantials: playging with compilir args on windows

--- conan_essentials/consumer-nogen/history.ps1
+++ conan_essentials/consumer-nogen/history.ps1
@@ -1,3 +1,8 @@
+# cl:
 conan install . -g compiler_args -pr windows-msvc-16-static-release-x64
+cl /EHsc timer.cpp `@conanbuildinfo.args
 
+# gcc/clang:
+conan install . -g compiler_args -pr windows-mingw-release-x64 --build=missing
+g++ timer.cpp `@conanbuildinfo.args -o bin/timer
 
