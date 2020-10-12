7605a2d5f3c42977b56117a46dc02ad3e83e0c1f
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-10-12 04:16:35 +0300

    Conan Essiantials: creating a package with scm attribute

---
+++ conan_essentials/create_package_scm/conanfile.py
@@ -0,0 +1,32 @@
+from conans import ConanFile, CMake
+
+
+class HelloConan(ConanFile):
+    name = "hello"
+    version = "0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+    scm = {
+        "type": "git",
+        "subfolder": "hello",
+        "url": "https://github.com/conan-io/hello.git",
+        "revision": "master"
+     }
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="hello")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="hello")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
---
+++ conan_essentials/create_package_scm/history.ps1
@@ -0,0 +1,3 @@
+conan new hello/0.1 -t -s
+conan create . d-k-ivanov/testing
+conan create . d-k-ivanov/testing -s build_type="Debug"
