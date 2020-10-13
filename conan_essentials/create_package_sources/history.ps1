88931953d7c2b58e6cd54a81e853d21123fbd6d2
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-10-13 03:58:32 +0300

    Conan Essiantials: building shared libraries with options

--- conan_essentials/create_package_sources/history.ps1
+++ conan_essentials/create_package_sources/history.ps1
@@ -1,3 +1,8 @@
 conan new hello/0.1 -t -s
 conan create . d-k-ivanov/testing -pr windows-msvc-16-static-release-x64
 conan create . d-k-ivanov/testing -pr windows-msvc-16-static-debug-x64
+
+
+conan create . d-k-ivanov/testing
+conan create . d-k-ivanov/testing -o hello:shared=True
+conan create . d-k-ivanov/testing -o hello:shared=True -s build_type=Debug
