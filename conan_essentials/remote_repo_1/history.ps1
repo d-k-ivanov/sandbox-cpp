014a05a9a0401541af04215992c0e61851693e1f
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-10-13 03:33:05 +0300

    Conan Essiantials: uploading to remote repo 2

--- conan_essentials/remote_repo_1/history.ps1
+++ conan_essentials/remote_repo_1/history.ps1
@@ -10,3 +10,13 @@ conan remote list
 conan upload "hello*" -r conan-divanov
 conan search "*" -r conan-divanov
 conan search hello/0.1@d-k-ivanov/testing -r conan-divanov
+
+conan upload "hello*" -r conan-divanov --all --confirm
+conan search "*" -r conan-divanov
+
+# Upload all
+conan upload "*" -r conan-divanov --all --confirm
+
+# Removing conan-center
+# (DON'T DO) conan remote remove conan-center
+# conan remote add conan-center https://conan.bintray.com
