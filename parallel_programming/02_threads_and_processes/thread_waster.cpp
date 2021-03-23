5396cc519f75a89ffd1c49821ef21298f9ff73d6
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-03-23 01:45:44 +0100

    Parallel CPP: playing with thread id

--- parallel_programming/02_threads_and_processes/thread_waster.cpp
+++ parallel_programming/02_threads_and_processes/thread_waster.cpp
@@ -5,15 +5,15 @@
 // a simple function that wastes CPU cycles "forever"
 void cpu_waster()
 {
-    printf("CPU Waster Process ID: %d\n", getpid());
-    printf("CPU Waster Thread ID %d\n", std::this_thread::get_id());
+    printf("CPU Waster Process ID: %d\n", _getpid());
+    printf("CPU Waster Thread ID %zu\n", std::hash<std::thread::id>{}(std::this_thread::get_id()));
     while(true) continue;
 }
 
 int main()
 {
-    printf("Main Process ID: %d\n", getpid());
-    printf("Main Thread ID: %d\n", std::this_thread::get_id());
+    printf("Main Process ID: %d\n", _getpid());
+    printf("Main Thread ID: %zu\n", std::hash<std::thread::id>{}(std::this_thread::get_id()));
     std::thread thread1(cpu_waster);
     std::thread thread2(cpu_waster);
 
