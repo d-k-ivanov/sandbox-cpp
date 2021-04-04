0c9eec7d387e3f9f6ea10a26c2794c264b0678fd
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-04-05 00:58:13 +0200

    Parallel CPP: playing with condition variable

--- parallel_programming/06_synchronization/condition_variable.cpp
+++ parallel_programming/06_synchronization/condition_variable.cpp
@@ -1,23 +1,33 @@
+#include <condition_variable>
 #include <cstdio>
 #include <mutex>
 #include <thread>
 
 int shared_data = 10;
 std::mutex mutex;
+std::condition_variable shared_data_taken;
 
-void hungry_person(int id)
+void take_some_data(int id)
 {
     int wasted_cycles = 0;
     while (shared_data > 0)
     {
         std::unique_lock<std::mutex> lock(mutex);
-        if ((id == shared_data % 2) && (shared_data > 0))
-        {
-            shared_data--;
-        }
-        else
+        // if ((id == shared_data % 5) && (shared_data > 0))
+        //     shared_data--;
+        // else
+        //     wasted_cycles++;
+
+        while ((id != shared_data % 5) && (shared_data > 0))
         {
             wasted_cycles++;
+            shared_data_taken.wait(lock);
+        }
+        if (shared_data > 0)
+        {
+            shared_data--;
+            lock.unlock();
+            shared_data_taken.notify_all();
         }
     }
     printf("Thread %d wasted %u cycles.\n", id, wasted_cycles);
@@ -25,9 +35,9 @@ void hungry_person(int id)
 
 int main()
 {
-    std::thread hungry_threads[2];
-    for (int i = 0; i < 2; i++)
-        hungry_threads[i] = std::thread(hungry_person, i);
+    std::thread hungry_threads[5];
+    for (int i = 0; i < 5; i++)
+        hungry_threads[i] = std::thread(take_some_data, i);
 
     for (auto& ht : hungry_threads)
         ht.join();
