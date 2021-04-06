0b285627005e2ee72caa633410d511b3e4992c92
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-04-06 12:12:35 +0200

    Parallel CPP: playing with recursive sum

--- parallel_programming/08_async/recursive_sum.cpp
+++ parallel_programming/08_async/recursive_sum.cpp
@@ -2,7 +2,25 @@
 
 #include "timer.h"
 
-unsigned long long recursive_sum(unsigned int lo , unsigned int hi, unsigned int depth=0)
+unsigned long long recursive_sum_one(unsigned int lo , unsigned int hi)
+{
+    if (hi - lo <= 100)
+    {
+        unsigned long long sum = 0;
+        for (auto i = lo; i < hi; i++)
+            sum += i;
+        return sum;
+    }
+    else
+    {
+        auto mid    = (hi + lo) / 2;
+        auto left   = recursive_sum_one(lo, mid);
+        auto right  = recursive_sum_one(mid, hi);
+        return left + right;
+    }
+}
+
+unsigned long long recursive_sum_two(unsigned int lo , unsigned int hi, unsigned int depth=0)
 {
     if (depth > 3)
     {
@@ -14,8 +32,8 @@ unsigned long long recursive_sum(unsigned int lo , unsigned int hi, unsigned int
     else
     {
         auto mid    = (hi + lo) / 2;
-        auto left   = recursive_sum(lo, mid, depth+1);
-        auto right  = recursive_sum(mid, hi, depth+1);
+        auto left   = recursive_sum_two(lo, mid, depth+1);
+        auto right  = recursive_sum_two(mid, hi, depth+1);
         return left + right;
     }
 }
@@ -23,7 +41,7 @@ unsigned long long recursive_sum(unsigned int lo , unsigned int hi, unsigned int
 int main()
 {
     Timer timer;
-    unsigned long long total = recursive_sum(0, 1000000000);
+    unsigned long long total = recursive_sum_one(0, 1000000000);
     printf("Total: %lld\n", total);
     printf("Elapsed time: %lld ms\n", timer.elapsed_time());
 }
