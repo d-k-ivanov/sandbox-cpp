f0dec3e5fcb53ec8eaf7bd9fccab8d5dd6bd4021
Dmitry Ivanov <d.k.ivanov@live.com>
2021-12-15 15:12:43 +0100

    playing with cherno-cpp-series

---
+++ cherno/src/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================
+ *      Project :  cherno-cpp-series
+ *      File    :  main.cpp
+ *      Created :  15.12.2021
+ *      Author  :  Dmitry Ivanov
+ * =====================================================================
+ */
+
+#include "main.h"
+
+#include <functional>
+#include <iostream>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+int main(int argc, char* argv[], char* env[])
+ {
+    // To turn off messages about unused variables.
+    ((void)argc );
+    ((void)argv );
+    ((void)env );
+
+    #ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    #endif
+
+    // std::system("pause");
+    return 0;
+ }
---
+++ cherno/src/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * =====================================================================
+ *      Project :  cherno-cpp-series
+ *      File    :  main.h
+ *      Created :  15.12.2022
+ *      Author  :  Dmitry Ivanov
+ * =====================================================================
+ */
+
+#endif //MAIN_H
