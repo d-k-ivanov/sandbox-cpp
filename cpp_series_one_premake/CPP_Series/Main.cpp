5a34689e3593924cccd8b6b7a50245724dd6b97f
Dmitry Ivanov <d.k.ivanov@live.com>
2021-12-19 01:29:42 +0100

    cpp_series_one_premake: 2021-19-12-01-29

--- cpp_series_one_premake/CPP_Series/Main.cpp
+++ cpp_series_one_premake/CPP_Series/Main.cpp
@@ -7,7 +7,8 @@
 #include <Windows.h>
 #endif
 
-int main(int argc, char* argv[], char* env[])
+// int main(int argc, char* argv[], char* env[])
+int main()
 {
     Log("Hello World!");
     std::cin.get();
