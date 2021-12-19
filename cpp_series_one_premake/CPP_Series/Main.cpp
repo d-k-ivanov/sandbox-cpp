a733ce349f7ad090cbf25d2536e023de33656973
Dmitry Ivanov <d.k.ivanov@live.com>
2021-12-20 00:24:27 +0100

    cpp_series_one_premake: 2021-20-12-00-24

--- cpp_series_one_premake/CPP_Series/Main.cpp
+++ cpp_series_one_premake/CPP_Series/Main.cpp
@@ -1,5 +1,6 @@
 #include "Main.h"
 #include "Log.h"
+#include "Math.h"
 
 #include <iostream>
 
@@ -10,8 +11,23 @@
 // int main(int argc, char* argv[], char* env[])
 int main()
 {
-    Log("Hello World!");
-    std::cin.get();
+    int a = 5;
+    int b = 8;
+    a++;
+    const char* str = "Hello World!";
+
+
+    for (int i = 0; i < strlen(str); ++i)
+    {
+        const char c = str[i];
+        std::cout << c << std::endl;
+
+    }
+
+    Log(str);
+    const int res = Multiply(a, b);
+    std::cout << res << std::endl;
+    // std::cin.get();
 
     // std::system("pause");
     return 0;
