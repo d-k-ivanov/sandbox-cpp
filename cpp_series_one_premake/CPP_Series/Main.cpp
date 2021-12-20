8bd7d4c34ae6d64603c80b2ca56d9d906e0705a8
Dmitry Ivanov <d.k.ivanov@live.com>
2021-12-20 01:06:29 +0100

    cpp_series_one_premake: 2021-20-12-01-06

--- cpp_series_one_premake/CPP_Series/Main.cpp
+++ cpp_series_one_premake/CPP_Series/Main.cpp
@@ -14,7 +14,20 @@ int main()
     int a = 5;
     int b = 8;
     a++;
+    b++;
     const char* str = "Hello World!";
+    const char* ptr = nullptr;
+
+    if(str)
+        Log(str);
+
+    if (ptr)
+        Log(str);
+    else
+        if (ptr)
+            Log(str);
+    else
+        Log("nullptr");
 
 
     for (int i = 0; i < strlen(str); ++i)
@@ -24,7 +37,6 @@ int main()
 
     }
 
-    Log(str);
     const int res = Multiply(a, b);
     std::cout << res << std::endl;
     // std::cin.get();
