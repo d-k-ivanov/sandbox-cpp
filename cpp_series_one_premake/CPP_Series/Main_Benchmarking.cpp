29711e03c67dc0ea8081053abafd589c9a2b8ae7
Dmitry Ivanov <d.k.ivanov@live.com>
2022-01-03 01:04:12 +0100

    cpp_series_one_premake: 2022-03-01-01-04

--- cpp_series_one_premake/CPP_Series/Main_Benchmarking.cpp
+++ cpp_series_one_premake/CPP_Series/Main_Benchmarking.cpp
@@ -8,7 +8,12 @@
 
 namespace MainBenchmarking
 {
-    void Main()
+    struct Vector2
+    {
+        float x, y;
+    };
+
+    void F1()
     {
         int value = 0;
         // __debugbreak(); // Visual Studio Breakpoint
@@ -18,42 +23,51 @@ namespace MainBenchmarking
                 value += 2;
         }
         std::cout << value << std::endl;
+    }
+
+    void F2()
+    {
+        std::array<std::shared_ptr<Vector2>, 1000> sharedPtrs1;
+        Timer timer("Make Shared -> 1000 Vector2");
+        for (auto& pointer : sharedPtrs1)
+            pointer = std::make_shared<Vector2>();
+    }
+
+    void F3()
+    {
+        std::array<std::shared_ptr<Vector2>, 1000> sharedPtrs2;
+        Timer timer("New  Shared -> 1000 Vector2");
+        for (auto& pointer : sharedPtrs2)
+            // ReSharper disable once CppSmartPointerVsMakeFunction
+            pointer = std::shared_ptr<Vector2>(new Vector2());
+    }
+
+    void F4()
+    {
+        std::array<std::unique_ptr<Vector2>, 1000> uniquePtrs1;
+        Timer timer("Make Unique -> 1000 Vector2");
+        for (auto& pointer : uniquePtrs1)
+            pointer = std::make_unique<Vector2>();
+    }
+
+    void F5()
+    {
+        std::array<std::unique_ptr<Vector2>, 1000> uniquePtrs2;
+        Timer timer("New  Unique -> 1000 Vector2");
+        for (auto& pointer : uniquePtrs2)
+            // ReSharper disable once CppSmartPointerVsMakeFunction
+            pointer = std::unique_ptr<Vector2>(new Vector2());
+    }
+
+    void Main()
+    {
+        F1();
 
         std::cout << "---------------------\n";
 
-        struct Vector2
-        {
-            float x, y;
-        };
-
-        {
-            std::array<std::shared_ptr<Vector2>, 10000> sharedPtrs1;
-            Timer timer("Make Shared -> 10000 Vector2");
-            for (auto& pointer : sharedPtrs1)
-                pointer = std::make_shared<Vector2>();
-        }
-
-        {
-            std::array<std::shared_ptr<Vector2>, 10000> sharedPtrs2;
-            Timer timer("New  Shared -> 10000 Vector2");
-            for (auto& pointer : sharedPtrs2)
-                // ReSharper disable once CppSmartPointerVsMakeFunction
-                pointer = std::shared_ptr<Vector2>(new Vector2());
-        }
-
-        {
-            std::array<std::unique_ptr<Vector2>, 10000> uniquePtrs1;
-            Timer timer("Make Unique -> 10000 Vector2");
-            for (auto& pointer : uniquePtrs1)
-                pointer = std::make_unique<Vector2>();
-        }
-
-        {
-            std::array<std::unique_ptr<Vector2>, 10000> uniquePtrs2;
-            Timer timer("New  Unique -> 10000 Vector2");
-            for (auto& pointer : uniquePtrs2)
-                // ReSharper disable once CppSmartPointerVsMakeFunction
-                pointer = std::unique_ptr<Vector2>(new Vector2());
-        }
+        F2();
+        F3();
+        F4();
+        F5();
     }
 }
