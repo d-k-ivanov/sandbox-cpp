976121953e317962cda9a47ae46cc8f3216a8fce
Dmitry Ivanov <d.k.ivanov@live.com>
2022-01-04 03:01:14 +0100

    cpp_series_one_premake: 2022-04-01-03-01

--- cpp_series_one_premake/CPP_Series/Main.cpp
+++ cpp_series_one_premake/CPP_Series/Main.cpp
@@ -5,14 +5,7 @@
 // int main(int argc, char* argv[], char* env[])
 int main()
 {
-    // MainArena::Main();
-    const int a = 5;
-    printf("%i\n", a);
-    printf("%i\n", a << 1);
-    printf("%i\n", a >> 1);
-    printf("%i\n", a << 4);
-
-
+    MainArena::Main();
     // std::system("pause");  // NOLINT(concurrency-mt-unsafe)
     return 0;
 }
