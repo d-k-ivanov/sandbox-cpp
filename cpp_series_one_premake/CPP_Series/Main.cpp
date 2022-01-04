da99f5d194c36e80d0389bbec70528aaa0b5978c
Dmitry Ivanov <d.k.ivanov@live.com>
2022-01-04 20:04:41 +0100

    cpp_series_one_premake: 2022-04-01-20-04 Iterators

--- cpp_series_one_premake/CPP_Series/Main.cpp
+++ cpp_series_one_premake/CPP_Series/Main.cpp
@@ -3,7 +3,7 @@
 // int main(int argc, char* argv[], char* env[])
 int main()
 {
-    MainStlAlgorithms::Main();
+    MainIterators::Main();
     // std::system("pause");  // NOLINT(concurrency-mt-unsafe)
     return 0;
 }
--- cpp_series_one_premake/CPP_Series/Main_Iterators.cpp
+++ cpp_series_one_premake/CPP_Series/Main_Iterators.cpp
@@ -1,11 +1,29 @@
+#include <array>
+#include <deque>
+#include <forward_list>
 #include <iostream>
+#include <map>
+#include <set>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #define P(x) std::cout << x << std::endl
 
 namespace MainIterators
 {
+    template<typename T>
+    std::string get_iterator_type(T it) {
+
+        if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::input_iterator_tag))	  return "Input";
+        else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::output_iterator_tag))        return "Output";
+        else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::forward_iterator_tag))       return "Forward";
+        else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::bidirectional_iterator_tag)) return "Bidirectional";
+        else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::random_access_iterator_tag)) return "Random Access";
+
+        return "missing";
+    }
+
     void Main()
     {
         {
@@ -22,6 +40,7 @@ namespace MainIterators
                 P(*it);
             std::cout << "------------------------------------------\n";
         }
+
         {
             using VectorMap = std::unordered_map<char, float>;
             using VectorMapIt = VectorMap::iterator;
@@ -38,6 +57,46 @@ namespace MainIterators
             for (auto [key, value] : map)
                 P(key << ": " << value);
             std::cout << "------------------------------------------\n";
+        }
+
+        {
+            std::array<int, 1> a;
+            std::vector<int> v;
+            std::deque<int> d;
+            std::forward_list<int> fl;
+            std::list<int> l;
+
+            std::set<int> s;
+            std::map<int, int> m;
+
+            std::unordered_map<int, int> um;
+            std::unordered_set<int, int> us;
+
+            auto it = a.begin();
+            auto it1 = v.begin();
+            auto it2 = d.begin();
+            auto it3 = fl.begin();
+            auto it4 = l.begin();
+
+            auto it5 = s.begin();
+            auto it6 = m.begin();
+
+            auto it7 = us.begin();
+            auto it8 = um.begin();
+
+            std::cout << "\nSequence Containers:\n\n";
+            std::cout << "array::begin         () returns a " << get_iterator_type(it) << " Iterator.\n";
+            std::cout << "vector::begin        () returns a " << get_iterator_type(it1) << " Iterator.\n";
+            std::cout << "deque::begin         () returns a " << get_iterator_type(it2) << " Iterator.\n";
+            std::cout << "forward_list::begin  () returns a " << get_iterator_type(it3) << " Iterator.\n";
+
+            std::cout << "\nAssociative Containers:\n\n";
+            std::cout << "set::begin           () returns a " << get_iterator_type(it5) << " Iterator.\n";
+            std::cout << "map::begin           () returns a " << get_iterator_type(it6) << " Iterator.\n";
+
+            std::cout << "\nUnordered Associative Containers:\n\n";
+            std::cout << "unordered_map::begin () returns a " << get_iterator_type(it7) << " Iterator.\n";
+            std::cout << "unordered_set::begin () returns a " << get_iterator_type(it8) << " Iterator.\n";
         }
     }
 }
