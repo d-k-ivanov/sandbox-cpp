a667791bc5fdc0a2061792aea141c1d328b7c660
Dmitry Ivanov <d.k.ivanov@live.com>
2022-01-18 09:26:29 +0100

    cpp_series_one_premake: 2022-18-01-09-26

--- cpp_series_one_premake/CPP_Series/Main_Bytes.cpp
+++ cpp_series_one_premake/CPP_Series/Main_Bytes.cpp
@@ -3,18 +3,42 @@
 
 namespace MainBytes
 {
-    template <class Dst, class Src>
-    Dst bitCast(const Src& source)
+    // template <class Dst, class Src>
+    // Dst bitCast(const Src& source)
+    // {
+    //     static_assert(sizeof(Dst) == sizeof(Src),
+    //         "Types have different data sizes. The byte cast is possible only for the types of the same size");
+    //     Dst dest{};
+    //     memcpy(&dest, &source, sizeof(dest));
+    //     return dest;
+    // }
+
+    template <class Target, class Source>
+    static Target bitCast(const Source& source)
     {
-        static_assert(sizeof(Dst) == sizeof(Src),
-            "Types have different data sizes. The byte cast is possible only for the types of the same size");
-        Dst dest{};
-        memcpy(&dest, &source, sizeof(dest));
-        return dest;
+        static_assert(sizeof(Target) == sizeof(Source),
+            "bitCast requires source and destination to be the same size");
+        static_assert(std::is_trivially_copyable<Target>::value,
+            "bitCast requires the destination type to be copyable");
+        static_assert(std::is_trivially_copyable<Source>::value,
+            "bitCast requires the source type to be copyable");
+
+        union BitCaster
+        {
+            explicit BitCaster(const Source& source) : src(source)
+            {
+            }
+
+            volatile Source src;
+            volatile Target dest;
+        };
+
+        return BitCaster(source).dest;
     }
 
     void Main()
     {
+        // std::bit_cast<float>(10);
         // ReSharper disable once CppVariableCanBeMadeConstexpr
         const uint32_t value1 = 1065353216;
         // float result = *reinterpret_cast<const float*>(&value1); // Aliasing Violation
